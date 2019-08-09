6bd6620c6ba7c4c9dcd0c233d9a15e9f71e0ef4d
Jonatan  Wallgren <jonatan.wallgren@gmail.com>
2019-08-09 13:42:19 +0200

    Changed reset to swap

--- src/reagent_test/core.cljs
+++ src/reagent_test/core.cljs
@@ -15,15 +15,16 @@
 
 (def app-state (local-storage (atom (hash-map)) :app-state))
 
-(defn edit-item [e item-id] (reset! app-state (assoc-in @app-state [item-id :editing] true)))
+(defn edit-item [e item-id] (swap! app-state assoc-in [item-id :editing] true))
 
-(defn todo-complete [item-id] (reset! app-state (assoc-in @app-state [item-id :completed] (not (:completed (get @app-state item-id))))))
+(defn todo-complete [item-id] (swap! app-state assoc-in [item-id :completed] (not (:completed (get @app-state item-id)))))
 
-(defn cancel-edit [e item-id] (reset! app-state (assoc-in @app-state [item-id :editing] false)))
+(defn cancel-edit [e item-id] (swap! app-state assoc-in [item-id :editing] false))
 
-(defn delete-item [item-id] (reset! app-state (dissoc @app-state item-id)))
+(defn delete-item [item-id] (swap! app-state dissoc item-id))
 
-(defn change-item [event item] (reset! app-state (assoc-in @app-state [(:id item) :title] (.-value (.-target event)))))
+(defn change-item [event item] (swap! app-state assoc-in [(:id item) :title] (.-value (.-target event))))
+
 
 (defn set-filter [filter] (reset! current-filter filter))
 
@@ -33,6 +34,7 @@
              (delete-item (first item))))))
 
 (defn main-section [state]
+  (println "state: " state)
   [:section {:class "todoapp"}
    [todo-header/component state]
    [:section {:class "main"}
@@ -41,7 +43,7 @@
              :type "checkbox"}]
     [:label {:htmlFor "toggle-all"} "Mark all as complete"]
     [:ul {:class "todo-list"}
-     (doall (for [item @app-state] 
+     (doall (for [item @app-state]
               (case @current-filter
                 ::all (todo-item/component (second item) edit-item cancel-edit change-item delete-item todo-complete)
                 ::active (when (not (:completed (second item))) (todo-item/component (second item) edit-item cancel-edit change-item delete-item todo-complete))
